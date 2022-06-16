42146af7f1520e088ffd00a5c31440c46831ce62
Mateus-Frnds <mateus.frnds@outlook.com>
2022-06-16 14:16:44 -0300

    add do ex1

---
+++ src/Exemplo1.java
@@ -0,0 +1,14 @@
+public class Exemplo1 {
+    public static void main(String[] args) {
+        
+        double B, b, h, area;
+
+        B = 8.0;
+        b = 6.0;
+        h = 5.0;
+
+        area = (B + b) / 2.0 * h;
+
+        System.out.println(area);
+    }
+}
